4dafe87f689e18b065b4fa78a1138b4b1267528c
lapaduga <deniskotov29042015@gmail.com>
2022-08-16 14:48:49 +0500

    layout is done

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,5 +1,5 @@
 import './MovieList.scss'
-import { Image, Card, Tag, Rate } from 'antd'
+import { Image, Card, Tag, Rate, Pagination } from 'antd'
 const { Meta } = Card
 
 export default function Search() {
@@ -11,133 +11,136 @@ export default function Search() {
   )
 
   return (
-    <ul className="movie-list">
-      <li>
-        <Image
-          width={183}
-          height={281}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
-          <Meta description="March 5, 2020 " />
-          <div className="tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
-          </div>
-          <p>
-            A former basketball all-star, who has lost his wife and family
-            foundation in a struggle with addiction attempts to regain his soul
-            and salvation by becoming the coach of a disparate ethnically mixed
-            high ...
-          </p>
-          <Rate count={10} allowHalf defaultValue={2.5} />
-        </Card>
-      </li>
-      <li>
-        <Image
-          width={183}
-          height={281}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
-          <Meta description="March 5, 2020 " />
-          <div className="tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
-          </div>
-          <p>
-            A former basketball all-star, who has lost his wife and family
-            foundation in a struggle with addiction attempts to regain his soul
-            and salvation by becoming the coach of a disparate ethnically mixed
-            high ...
-          </p>
-          <Rate count={10} allowHalf defaultValue={2.5} />
-        </Card>
-      </li>
-      <li>
-        <Image
-          width={183}
-          height={281}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
-          <Meta description="March 5, 2020 " />
-          <div className="tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
-          </div>
-          <p>
-            A former basketball all-star, who has lost his wife and family
-            foundation in a struggle with addiction attempts to regain his soul
-            and salvation by becoming the coach of a disparate ethnically mixed
-            high ...
-          </p>
-          <Rate count={10} allowHalf defaultValue={2.5} />
-        </Card>
-      </li>
-      <li>
-        <Image
-          width={183}
-          height={281}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
-          <Meta description="March 5, 2020 " />
-          <div className="tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
-          </div>
-          <p>
-            A former basketball all-star, who has lost his wife and family
-            foundation in a struggle with addiction attempts to regain his soul
-            and salvation by becoming the coach of a disparate ethnically mixed
-            high ...
-          </p>
-          <Rate count={10} allowHalf defaultValue={2.5} />
-        </Card>
-      </li>
-      <li>
-        <Image
-          width={183}
-          height={281}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
-          <Meta description="March 5, 2020 " />
-          <div className="tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
-          </div>
-          <p>
-            A former basketball all-star, who has lost his wife and family
-            foundation in a struggle with addiction attempts to regain his soul
-            and salvation by becoming the coach of a disparate ethnically mixed
-            high ...
-          </p>
-          <Rate count={10} allowHalf defaultValue={2.5} />
-        </Card>
-      </li>
-      <li>
-        <Image
-          width={183}
-          height={281}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
-          <Meta description="March 5, 2020 " />
-          <div className="tags">
-            <Tag>Action</Tag>
-            <Tag>Drama</Tag>
-          </div>
-          <p>
-            A former basketball all-star, who has lost his wife and family
-            foundation in a struggle with addiction attempts to regain his soul
-            and salvation by becoming the coach of a disparate ethnically mixed
-            high ...
-          </p>
-          <Rate count={10} allowHalf defaultValue={2.5} />
-        </Card>
-      </li>
-    </ul>
+    <>
+      <ul className="movie-list">
+        <li>
+          <Image
+            width={183}
+            height={281}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+          <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
+            <Meta description="March 5, 2020 " />
+            <div className="tags">
+              <Tag>Action</Tag>
+              <Tag>Drama</Tag>
+            </div>
+            <p>
+              A former basketball all-star, who has lost his wife and family
+              foundation in a struggle with addiction attempts to regain his
+              soul and salvation by becoming the coach of a disparate ethnically
+              mixed high ...
+            </p>
+            <Rate count={10} allowHalf defaultValue={2.5} />
+          </Card>
+        </li>
+        <li>
+          <Image
+            width={183}
+            height={281}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+          <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
+            <Meta description="March 5, 2020 " />
+            <div className="tags">
+              <Tag>Action</Tag>
+              <Tag>Drama</Tag>
+            </div>
+            <p>
+              A former basketball all-star, who has lost his wife and family
+              foundation in a struggle with addiction attempts to regain his
+              soul and salvation by becoming the coach of a disparate ethnically
+              mixed high ...
+            </p>
+            <Rate count={10} allowHalf defaultValue={2.5} />
+          </Card>
+        </li>
+        <li>
+          <Image
+            width={183}
+            height={281}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+          <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
+            <Meta description="March 5, 2020 " />
+            <div className="tags">
+              <Tag>Action</Tag>
+              <Tag>Drama</Tag>
+            </div>
+            <p>
+              A former basketball all-star, who has lost his wife and family
+              foundation in a struggle with addiction attempts to regain his
+              soul and salvation by becoming the coach of a disparate ethnically
+              mixed high ...
+            </p>
+            <Rate count={10} allowHalf defaultValue={2.5} />
+          </Card>
+        </li>
+        <li>
+          <Image
+            width={183}
+            height={281}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+          <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
+            <Meta description="March 5, 2020 " />
+            <div className="tags">
+              <Tag>Action</Tag>
+              <Tag>Drama</Tag>
+            </div>
+            <p>
+              A former basketball all-star, who has lost his wife and family
+              foundation in a struggle with addiction attempts to regain his
+              soul and salvation by becoming the coach of a disparate ethnically
+              mixed high ...
+            </p>
+            <Rate count={10} allowHalf defaultValue={2.5} />
+          </Card>
+        </li>
+        <li>
+          <Image
+            width={183}
+            height={281}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+          <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
+            <Meta description="March 5, 2020 " />
+            <div className="tags">
+              <Tag>Action</Tag>
+              <Tag>Drama</Tag>
+            </div>
+            <p>
+              A former basketball all-star, who has lost his wife and family
+              foundation in a struggle with addiction attempts to regain his
+              soul and salvation by becoming the coach of a disparate ethnically
+              mixed high ...
+            </p>
+            <Rate count={10} allowHalf defaultValue={2.5} />
+          </Card>
+        </li>
+        <li>
+          <Image
+            width={183}
+            height={281}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+          <Card hoverable title={elem} bordered={false} style={{ width: 268 }}>
+            <Meta description="March 5, 2020 " />
+            <div className="tags">
+              <Tag>Action</Tag>
+              <Tag>Drama</Tag>
+            </div>
+            <p>
+              A former basketball all-star, who has lost his wife and family
+              foundation in a struggle with addiction attempts to regain his
+              soul and salvation by becoming the coach of a disparate ethnically
+              mixed high ...
+            </p>
+            <Rate count={10} allowHalf defaultValue={2.5} />
+          </Card>
+        </li>
+      </ul>
+      <Pagination size="small" total={50} />
+    </>
   )
 }
